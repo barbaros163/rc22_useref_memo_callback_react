a326f068ddbf10560712eeb8cb0417609cb80d1a
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-10 13:58:34 +0100

    some codes added

--- useref-memo-callback/src/components/Header.jsx
+++ useref-memo-callback/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ count }) => {
+  console.log("Render => Header Components");
+  return (
+    <div className="bg-warning text-center">Header Componenti: {count}</div>
+  );
 };
 
 export default Header;
