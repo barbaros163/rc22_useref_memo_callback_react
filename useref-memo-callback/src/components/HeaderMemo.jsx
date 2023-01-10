5b82f5ba7c26cee791f9907360ac1bdbadc84163
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-10 23:42:10 +0100

    sca

--- useref-memo-callback/src/components/HeaderMemo.jsx
+++ useref-memo-callback/src/components/HeaderMemo.jsx
@@ -3,7 +3,7 @@ import React, { memo } from "react";
 const HeaderMemo = ({ count }) => {
   console.log("Render => HeaderMemo Components");
   return (
-    <div className="bg-danger text-center">HeaderMemo Componenti: {count} </div>
+    <div className="bg-danger text-center">HeaderMemo Componenti : {count}</div>
   );
 };
 
