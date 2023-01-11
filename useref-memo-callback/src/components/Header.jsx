11266573cb678be30c9003260b0823e99ecb7170
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-11 12:04:03 +0100

    done

--- useref-memo-callback/src/components/Header.jsx
+++ useref-memo-callback/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({ count }) => {
   console.log("Render => Header Components");
   return (
-    <div className="bg-warning text-center">Header Componenti: {count}</div>
+    <div className="bg-warning text-center">Header Componenti : {count}</div>
   );
 };
 
